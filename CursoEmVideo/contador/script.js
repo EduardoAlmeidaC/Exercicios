e375df24c0421a1c4ecaa00a8e273ad44aaa452f
EduardoAlmeidaC <eduardo.c.almeida91@gmail.com>
2022-11-30 17:46:01 -0300

    Contador
    Este exercício pega três entradas numéricas do usuário e realiza uma contagem crescente ou decrescente. Para definir se a contagem vai crescer ou decrescer o código avalia se o primeiro número é maior ou menor do que o segundo número.

---
+++ CursoEmVideo/contador/script.js
@@ -0,0 +1,34 @@
+function contar() {
+    let ini = document.getElementById('txti');
+    let fim = document.getElementById('txtf');
+    let passo = document.getElementById('txtp');
+    let res = document.getElementById('res');
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+       // window.alert('[ERRO] Faltam dados!');
+        res.innerHTML = 'Impossível Contar!';
+    } else {
+        res.innerHTML = 'Contando: </br>';
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+        if (p<=0) {
+            window.alert('Passo inválido! Considerando PASSO 1');
+            p = 1;
+        }
+
+        if (i<f) {
+            // Contagem Crescente.
+          for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`;  
+            } 
+        } else {
+            // Contagem Decrescente.
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`;  
+            }          
+        }
+
+        res.innerHTML += `\u{1F3C1}`;
+    }
+}
